f11ba7fe4010a3f09d6c67fb6055577a9623b6f5
Vadym <vadimkiricenko572@gmail.com>
2026-04-30 01:42:31 +0300

    кукуа

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -4,3 +4,8 @@ export const nextServer = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   withCredentials: true,
 });
+
+export const internalApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  withCredentials: true,
+});
